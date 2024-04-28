5c7d24ee835dc70f589b36ccf0f789890c7ccf16
Estera-Mieszczak <144910099+Estera-Mieszczak@users.noreply.github.com>
2024-04-28 17:50:26 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,81 @@
+import './App.css';
+import {  useEffect, useState } from "react";
+import { nanoid } from 'nanoid';
+import { ContactList } from './ContactList/ContactList';
+import { Form } from './Form/Form';
+import { Section } from './Section/Section';
+
+const INITCONTACTS = {
+  contacts: [],
+  filter: '',
+  name: '',
+  number:'',
+}
+
 export const App = () => {
+  const getData = () => {
+    const initialValue = JSON.parse(localStorage.getItem("contacts"));
+    
+      return (initialValue ? initialValue : INITCONTACTS);
+  };
+  useEffect(() => {
+    
+    console.log(getData());
+  }, []);
+
+const [dataPhonebook, setDataPhonebook] = useState(getData());
+
+
+  const setData = () => {
+    localStorage.setItem("contacts", JSON.stringify(dataPhonebook));
+  };
+
+  
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setDataPhonebook({...dataPhonebook,[name]: value })
+   };
+    
+  const onSubmit = (event) => {
+    const { name, number, contacts} = dataPhonebook;
+    event.preventDefault();
+    document.getElementById("nameContact").value = '';
+    document.getElementById("numberContact").value = '';
+
+const isExist = contacts.some(
+      contact => contact.name.toLowerCase() === name.trim().toLowerCase()
+    );
+    if (isExist) {
+      alert(`${name} is in contacts`);
+      return;
+    }
+
+    setDataPhonebook({
+      ...dataPhonebook,
+      contacts: [...contacts, { id: nanoid(), name, number }],
+      filter:'',
+      name: '',
+      number: ''
+    });
+    console.log(dataPhonebook)
+    
+  };
+
+setData();
+  const onDelete = (contactId) => {
+    setDataPhonebook({
+      ...dataPhonebook,
+      contacts: dataPhonebook.contacts.filter(contact => contact.id !== contactId)
+    });
+  };
+    
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div className='divForm'> 
+      <Section title="Phonebook" children={<Form dataPhonebook={dataPhonebook} onSubmit={onSubmit} onChange={onChange}></Form>}></Section>
+        <Section title="Contacts" children={<ContactList dataPhonebook={dataPhonebook} onDelete={onDelete} onChange={onChange}></ContactList>}></Section>
+        </div>
+    </>
   );
 };
